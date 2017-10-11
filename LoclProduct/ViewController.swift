64ee4662ac4dd93e814c461b460d8e5f8dfff121
Federica Luraschi <federica.luraschi@gmail.com>
2017-10-11 10:18:37 +0900

    Initial Commit

---
+++ LoclProduct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoclProduct
+//
+//  Created by Federica Luraschi on 2017-10-11.
+//  Copyright © 2017 Federica Luraschi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
