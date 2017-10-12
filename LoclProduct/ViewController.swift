f680eec843cc699e190727581353273f83a2f162
Federica Luraschi <federica.luraschi@gmail.com>
2017-10-12 18:19:25 +0900

    v1.0

--- LoclProduct/ViewController.swift
+++ LoclProduct/ViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate  {
 
+    @IBOutlet var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let myURL = Bundle.main.url(forResource: "test/index", withExtension: "html")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+        }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
